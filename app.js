aad7f39e75df2a6fd5238c2b559c07d27f9b2da4
Vladimir <vladimirrogalev1@gmail.com>
2024-11-28 19:04:53 +0200

    commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var url = require('url')
+
+
+const server = http.createServer(function (req, res) {
+    if (req.url == '/') {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.end('<h1>Hello World! Добро пожаловать!! שלום! </h1>');
+    }
+    if (req.url == '/time') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        // var now = new Date();
+        res.end(`<h1><time>${new Date()}</time></h1>`);
+    }
+    if (req.url.startsWith('/hello?name') ) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        let name = url.parse(req.url,true);
+        res.end(`<h1>Hello ${name}</h1>`);
+    }
+
+
+});
+
+server.listen(9191, () => {
+    console.log("server is ready : http://localhost:9191/");
+})
+
+
